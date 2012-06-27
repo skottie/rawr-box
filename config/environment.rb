ab31b29a3c41cbe529092cbd5b8395757e344c71
Scott Conner <scottc@spiceworks.com>
2012-06-27 17:08:53 -0500

    added the branch controller, and a migration to start pulling local git repo's every 60 seconds once it is fully configured.

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 require 'delayed_job'
+require 'grit'
 
 require File.join(File.dirname(__FILE__), '../lib/scheduled_job')
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627205257) do
+ActiveRecord::Schema.define(:version => 20120627220429) do
 
   create_table "commits", :force => true do |t|
     t.string   "team_handle"
