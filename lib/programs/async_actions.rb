6b936e6c1ca94de14947e80f9a727d48c2bef4cd
Scott Conner <scottc@spiceworks.com>
2012-06-27 17:21:56 -0500

    changes async actions

--- lib/programs/async_actions.rb
+++ lib/programs/async_actions.rb
@@ -3,22 +3,22 @@ class AsyncActions
 	# voice command for playing daily progress
 	def self.play_progress
 		commit_count = Commit.today.count
-		AsyncActions.delay.delayed_command("say 'There has been #{commit_count} commits today.'")
+		command_line "say 'There has been #{commit_count} commits today.'"
 	end
 
 	# play an async sound
 	def self.play_sound(path, options = {})
 		volume = options[:volume] ? "-v #{options[:volume]}" : ""
-		AsyncActions.delay.delayed_command("afplay #{volume} #{path}")
+		command_line "afplay #{volume} #{path}"
 	end
 
 	# run an async command
 	def self.command_line(cmd)
-		AsyncActions.delay.delayed_command(cmd)	
+		AsyncActions.new.send_later(:delayed_command, cmd)
 	end
 
 	# where everything actually gets piped to
-	def self.delayed_command(cmd)
+	def delayed_command(cmd)
 		puts "command: #{cmd}"
 		response = `#{cmd}`
 		puts "response: #{response}"
