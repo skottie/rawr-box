4a55915698febc05d59823f45eeb994343509a73
Tim Gittos <timg@spiceworks.com>
2012-07-02 11:08:15 -0500

    Fixed test

--- test/unit/configuration_value_test.rb
+++ test/unit/configuration_value_test.rb
@@ -2,14 +2,9 @@ require 'test_helper'
 
 class ConfigurationValueTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-
   test "participants returns a list of participants" do
     ConfigurationValue.create_or_update('participants', 'foo, bar, baz')
-    assert_equal 3, ConfigurationValue.participants
+    assert_equal 3, ConfigurationValue.participants.length
     assert_equal 'foo', ConfigurationValue.participants.first
   end
 
