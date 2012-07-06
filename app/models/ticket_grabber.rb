aa7f6993c83676c10349142c684899718dfee6a1
Scott Conner <scottc@spiceworks.com>
2012-07-06 09:51:54 -0500

    adds the ticket author name to the ticket by adding a second join

--- app/models/ticket_grabber.rb
+++ app/models/ticket_grabber.rb
@@ -13,7 +13,7 @@ class TicketGrabber
 		db = PGconn.new('redmine', 5432, nil, nil, 'redmine', username, password)	
 
 		# grab the tickets we care about, and collate them with our own tickets
-		db.exec("SELECT issues.project_id, issues.id, issues.created_on, users.lastname, issues.subject, issues.description FROM issues, users WHERE issues.assigned_to_id = users.id AND users.lastname IN (#{last_names.join(',')})").each { |t| RedmineTicket.create_or_update(t) }
+		db.exec("SELECT authors.firstname as author_first, authors.lastname as author_last, issues.project_id, issues.id, issues.created_on, users.lastname, issues.subject, issues.description FROM issues, users, users as authors WHERE issues.author_id = authors.id AND issues.assigned_to_id = users.id AND users.lastname IN (#{last_names.join(',')})").each { |t| RedmineTicket.create_or_update(t) }
 		rescue Exception => e
 			`say "I failed to grab the tickets from redmine."`
 	end	
